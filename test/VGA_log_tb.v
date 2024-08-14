78790df7bf3c672aa43943e92b5ed0d94ee61ca6
James <162901067+Enjimneering@users.noreply.github.com>
2024-08-14 10:10:36 +0100

    Uploaded updated log file - poland example

--- test/VGA_log_tb.v
+++ test/VGA_log_tb.v
@@ -1,10 +1,9 @@
-//////////////////////////////////////////////////////////////////////////////
 /* 
-     Project: TinyTapeStation
-    Author(s):      James Ashie Kotey, Abdulatif Babli
+    Project: TinyTapeStation
+    Author(s):      James Ashie Kotey, Abdulatif Balbi
     Create Date:    10/08/2024
     Module Name:    VGA_Testbench
-    Summary:        6 control for 640x480 60Hz VGA 6-bit RGB 
+    Summary:        6 control for 640x480 60Hz VGA n-bit RGB 
     
     Description ==============================================================
     
@@ -12,32 +11,42 @@
     <> = bINARY
     {} = decimal
 
+    This testbench provides a simple framework for generating VGA patterns outputs to be tested.
+    currently, the input accomodates 6-bit rgb This can be modified/ increased easily. 
+
     1 CLK Cyclce = 40ns - 25MHz CLK
 */
-//////////////////////////////////////////////////////////////////////////////
+
 
 `timescale 1ns / 1ns
 `include "VGA_Top.v"  // reference  to top module not needed in sim
 
 module VGA_Testbench;
 
-integer output_file;
-integer i;
+    integer output_file;
+    integer i;
 
-reg CLK, RESET;
-reg [5:0] DATA_IN;
-wire H_SYNC, V_SYNC;
-wire [5:0] RGB_OUT;
+    reg CLK, RESET;
+    reg [15:0] DATA_IN;         
+    wire H_SYNC, V_SYNC;
+    wire [15:0] RGB_OUT;
 
-    VGA_Top vga (              // TGA TOP module needs these ports
+    VGA_Top vga (               
     .pixel_clk(CLK),            // IN
     .reset(RESET),              // IN
     .color_data(DATA_IN),       // IN
     .h_sync(H_SYNC),            // OUT
     .v_sync(V_SYNC),            // OUT
     .rgb(RGB_OUT)               // OUT
-);
+    );
 
+    // Change bus using these output ports
+
+    //  8-BIT - RRR GGG BB
+    wire [2:0] RED = RGB_OUT[7:5];
+    wire [2:0] GREEN = RGB_OUT[4:2];
+    wire [1:0] BLUE = RGB_OUT[1:0];
+    
 
     initial begin 
     $dumpfile("tb.vcd");
@@ -47,19 +56,29 @@ wire [5:0] RGB_OUT;
 
     CLK        = 0;                       // start clk from zero.           
     RESET      = 1;                       // send reset signal.
-    DATA_IN    = 'b001100;                // change this to change output color - RR GG BB
+    DATA_IN    = 'b0000_0000;                // change this to change output color - RRR GGG BB
 
-    $fdisplay(output_file,"%0t ns: %b %b %b %b %b", $time , H_SYNC, V_SYNC, RGB_OUT[5:4], RGB_OUT[3:2], RGB_OUT[1:0]); // print line to log.
+    
+    
+    $fdisplay(output_file,"%0t ns: %b %b %b %b %b", $time , H_SYNC, V_SYNC, RED, GREEN, BLUE); // print line to log.
    
     #40 RESET  = 0; // 40ns Clock = 25 MHz
-    $fdisplay(output_file,"%0t ns: %b %b %b %b %b", $time , H_SYNC, V_SYNC, RGB_OUT[5:4], RGB_OUT[3:2], RGB_OUT[1:0]); 
+    $fdisplay(output_file,"%0t ns: %b %b %b %b %b", $time , H_SYNC, V_SYNC, RED, GREEN, BLUE); 
 
-    for ( i = 0; i < 419202 ; i = i + 1 ) begin // loop for 1 full frame (800 * 524)
-        #40 $fdisplay(output_file,"%0t ns: %b %b %b %b %b ", $time, H_SYNC, V_SYNC, RGB_OUT[5:4], RGB_OUT[3:2], RGB_OUT[1:0]);
-        #40 $fdisplay(output_file, "%0t ns: %b %b %b %b %b", $time, H_SYNC, V_SYNC, RGB_OUT[5:4], RGB_OUT[3:2], RGB_OUT[1:0]);
+    for ( i = 0; i < 419218 ; i = i + 1 ) begin // loop for 1 full frame (800 * 524)
+        
+        if (i >  419220 /2)
+        DATA_IN = 'b1110_0000;
+        else
+        DATA_IN = 'b1111_1111;
+        
+
+        #40 $fdisplay(output_file,"%0t ns: %b %b %b %b %b ", $time, H_SYNC, V_SYNC, RED, GREEN, BLUE);
+        #40 $fdisplay(output_file, "%0t ns: %b %b %b %b %b", $time, H_SYNC, V_SYNC, RED, GREEN, BLUE);
     end
 
     $fclose(output_file);
+
     $display("Simulation Complete :D");
     $finish();
     end
