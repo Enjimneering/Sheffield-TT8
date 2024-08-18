cb89d31efa9f20b690d2bb19b0b5817827293cda
Kitsudora <bshi12@sheffield.ac.uk>
2024-08-18 13:53:54 +0800

    update Rom
    Corrected the dot matrix orientation of Gnome_2, Sheep_1, Sheep_2.

--- src/AssetROM.v
+++ src/AssetROM.v
@@ -31,7 +31,7 @@ function [7:0] romData;
     end
 
     case(charc_Func)
-        4'b0000: begin
+        4'b0000: begin  //Heart
             case(newIndex)
             3'b000: romData = 8'b11111111;
             3'b001: romData = 8'b10011001;
@@ -44,7 +44,7 @@ function [7:0] romData;
             endcase
         end
 
-        4'b0001: begin  //SwordVertical:
+        4'b0001: begin  //Sword
             case(newIndex)
             3'b000: romData = 8'b11101111;
             3'b001: romData = 8'b11101111;
@@ -57,7 +57,7 @@ function [7:0] romData;
             endcase
         end
 
-        4'b0010: begin 
+        4'b0010: begin  //Gnome_1
             case(newIndex)
             3'b000: romData = 8'b11111111;
             3'b001: romData = 8'b11000011;
@@ -70,21 +70,21 @@ function [7:0] romData;
             endcase
         end
 
-        4'b0011: begin 
-            case(newIndex)
-            3'b000: romData = 8'b11111111;
-            3'b001: romData = 8'b10001111;
-            3'b010: romData = 8'b10000011;
-            3'b011: romData = 8'b11000001;
-            3'b100: romData = 8'b10010101;
+        4'b0011: begin  //Dragon_1
+            case(newIndex) 
+            3'b000: romData = 8'b11111011;
+            3'b001: romData = 8'b11100011;
+            3'b010: romData = 8'b11001000;
+            3'b011: romData = 8'b11000011;
+            3'b100: romData = 8'b10001001;
             3'b101: romData = 8'b10000000;
-            3'b110: romData = 8'b10001011;
-            3'b111: romData = 8'b11011011;
+            3'b110: romData = 8'b10010001;
+            3'b111: romData = 8'b11111111;
             endcase
         end
 
-        4'b0100: begin 
-            case(newIndex)
+        4'b0100: begin  //Dragon_2
+            case(newIndex) 
             3'b000: romData = 8'b11001111;
             3'b001: romData = 8'b11100011;
             3'b010: romData = 8'b01000010;
@@ -96,8 +96,8 @@ function [7:0] romData;
             endcase
         end
 
-        4'b0101: begin 
-            case(newIndex)
+        4'b0101: begin  //Dragon_3
+            case(newIndex) 
             3'b000: romData = 8'b11111111;
             3'b001: romData = 8'b10000011;
             3'b010: romData = 8'b01000010;
@@ -109,8 +109,8 @@ function [7:0] romData;
             endcase
         end
 
-        4'b0110: begin 
-            case(newIndex)
+        4'b0110: begin  //Dragon_Head
+            case(newIndex) 
             3'b000: romData = 8'b10111111;
             3'b001: romData = 8'b11000111;
             3'b010: romData = 8'b00110000;
@@ -122,29 +122,29 @@ function [7:0] romData;
             endcase
         end
 
-        4'b0111: begin 
+        4'b0111: begin  //Sheep_1
             case(newIndex)
-            3'b000: romData = 8'b11100011;
-            3'b001: romData = 8'b10011101;
-            3'b010: romData = 8'b00011110;
-            3'b011: romData = 8'b00111110;
-            3'b100: romData = 8'b10111110;
-            3'b101: romData = 8'b10000001;
-            3'b110: romData = 8'b11011011;
-            3'b111: romData = 8'b11011011;
+            3'b000: romData = 8'b11001111;
+            3'b001: romData = 8'b10000011;
+            3'b010: romData = 8'b10011000;
+            3'b011: romData = 8'b01111011;
+            3'b100: romData = 8'b01111011;
+            3'b101: romData = 8'b01111000;
+            3'b110: romData = 8'b10111011;
+            3'b111: romData = 8'b11000111;
             endcase
         end
 
-        4'b1000: begin 
+        4'b1000: begin  //Sheep_2
             case(newIndex)
-            3'b000: romData = 8'b11111111;
-            3'b001: romData = 8'b11100011;
-            3'b010: romData = 8'b10011101;
-            3'b011: romData = 8'b00011110;
-            3'b100: romData = 8'b00111110;
-            3'b101: romData = 8'b10111110;
-            3'b110: romData = 8'b10000001;
-            3'b111: romData = 8'b11011011;
+            3'b000: romData = 8'b11100111;
+            3'b001: romData = 8'b11000001;
+            3'b010: romData = 8'b11001100;
+            3'b011: romData = 8'b10111101;
+            3'b100: romData = 8'b10111101;
+            3'b101: romData = 8'b10111100;
+            3'b110: romData = 8'b11011101;
+            3'b111: romData = 8'b11100011;
             endcase
         end
 
