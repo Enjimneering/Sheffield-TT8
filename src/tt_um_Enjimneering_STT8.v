4657727d75bb3f062c81a3c8eb29b2d7761ee454
James <162901067+Enjimneering@users.noreply.github.com>
2024-08-23 13:07:24 +0100

    fixed uio_in typo

--- src/tt_um_Enjimneering_STT8.v
+++ src/tt_um_Enjimneering_STT8.v
@@ -8,7 +8,7 @@
 module tt_um_Enjimneering_STT8 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] C,   // IOs: Input path
+    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
