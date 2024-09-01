b7061d375f02f9c498702463f4164b8e461fba6e
Kitsudora <bshi12@sheffield.ac.uk>
2024-09-01 23:58:26 +0800

    Update tt_um_Enjimneering_STT8.v

--- src/tt_um_Enjimneering_STT8.v
+++ src/tt_um_Enjimneering_STT8.v
@@ -34,7 +34,7 @@ module tt_um_Enjimneering_STT8 (
     .entity_4(entityInput),
     .entity_5(entityInput2),
     .entity_6(entityInput),
-    .entity_7(entityInput2),
+    .entity_7_Array(entityInput2),
     .entity_8_Flip(entityInput2),
     .entity_9_Flip(entityInput),
     .counter_V(counterInputV),
