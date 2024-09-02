6dfb5afbd85c631995c31f2990a981369e9507d3
Anubhav <anubhav.kumar.avinash@gmail.com>
2024-09-02 20:35:46 +0100

    Updated anubhav_player_logic

--- src/anubhav_player_logic.v
+++ src/anubhav_player_logic.v
@@ -15,7 +15,6 @@ Sword Logic
 
 Inputs 
     Controller Input:
-       
         A       - Attack
         B       - Attack
         UP      - Move UP
@@ -32,7 +31,7 @@ Inputs
 Outputs
 
     Next State Information
-    	New player location
+        New player location
     
     Location Format: 0000_0000  (X,Y) - tiles
 
@@ -45,28 +44,28 @@ Outputs
         Y+
 */
 
+//entity for player is 2, sword is 1
+// orientiant 00 = up, 01 = right, 10 = down, 11=left
+
+
 // Define Behaviour States 
 module player (
     input wire frame_clk,
     input wire rst,
     input wire A, B, select, start, up, down, left, right,
 
-    output reg [7:0] player_location,
-    output reg [7:0] sword_location 
-
+    output reg [13:0] player,
+    output reg [13:0] player_health,
+    output reg [13:0] sword
     /* we might need this if we're doing collision logic in each entity module
     input wire  [7:0]  sheep_location, 
-   	input wire  [7:0]  dragon_head_location,
+    input wire  [7:0]  dragon_head_location,
     input wire  [2:0]  dragon_head_direction,
-	*/
+    */
 );
     // State register
     reg [1:0] current_state;
     reg [1:0] next_state;
-
-    // Player Attributes
-    reg [1:0] player_health; // 00 = dead, no hearts; 01 = 1 heart; 10 = 2 hearts; 11 = 3 hearts
-    reg [1:0] player_direction; // Also corresponds to sword direction(00 = up, 01 = down, 10 = left, 11 = right)
 
     // State definitions
     localparam MOVE_STATE = 2'b00;  // Move when there is an input from the controller
@@ -75,71 +74,70 @@ module player (
     localparam DEAD = 2'b11;  // end game - player wins
 
     // Gamplay Flags
-	reg dragon_hurt; // 1 less body part or dead
-	reg player_hurt; // 1 less player heart and move two tiles into the direction it got hit or dead
-	reg dragon_dead; // VICTORY ACHIEVED 
-	reg player_dead; // YOU DIED
+    reg dragon_hurt; // 1 less body part or dead
+    reg player_hurt; // 1 less player heart and move two tiles into the direction it got hit or dead
+    reg dragon_dead; // VICTORY ACHIEVED 
+    reg player_dead; // YOU DIED
     reg sword_visible;
-	reg game_over;
-
-    always @(posedge frame_clk) begin
-        if (rst) begin   // reset logic
-            current_state <= IDLE_STATE;
-            player_location <= 8'b0100_0100; // Example stating location
-            player_health <= 2'b11; // Max health = 3 hearts
-            sword_location <= 8'b0000_0000; //Anywhere out of grid
-            sword_visible <= 1'b0;
-        end 
-
-        else if (!game_over) current_state = next_state;
-    end
+    reg game_over;        
 
     // Decide what the current state should be    
     always @(posedge frame_clk) begin
-
+        if (rst) begin   // reset logic
+            current_state <= IDLE_STATE;
+            player <= 14'b0010_01_1000_0010; // Example stating location entityId_orientation_XXXX_YYYY
+            player_health <= 2'b11; // Max health = 3 hearts
+            sword <= 14'b1111_01_0000_0000; //Anywhere out of grid entityId_orientation_XXXX_YYYY
+            sword_visible <= 1'b0;
+        end else if (!game_over) current_state = next_state;
+    
         case (current_state)
             IDLE_STATE: begin
-                if (up || down || left || right)
-                    next_state = MOVE_STATE;
-                else if (A)
-                    next_state = ATTACK_STATE;
-                else if (player_health == 0)
+                if (up ^ down ^ left ^ right) begin
+                    if (A || B)
+                        next_state = ATTACK_STATE;
+                    else 
+                        next_state = MOVE_STATE;
+                end else if (player_health == 0)
                     next_state = DEAD;
                 else
                     next_state = IDLE_STATE;
             end
 
             MOVE_STATE: begin
-        		if (up) begin
-                    player_location <= player_location - 8'b0000_1000;
-                    player_direction <= 2'b00;
-                end else if (down) begin
-                    player_location <= player_location + 8'b0000_1000;
-                    player_direction <= 2'b01;
-                end else if (left) begin
-                    player_location <= player_location - 8'b1000_0000;
-                    player_direction <= 2'b10;
-                end else if (right) begin
-                    player_location <= player_location + 8'b1000_0000;
-                    player_direction <= 2'b11;
+                if (up && player [3:0] != 0010) begin //also checking that the player isn't at the border
+                    player [3:0] <= player [3:0] - 4'b0001; //location change in tiles
+                end else if (down && player [3:0] != 1010) begin
+                    player [3:0] <= player [3:0] + 4'b0001; 
+                end else if (left && player [7:4] != 0001) begin
+                    player [7:4] <= player [7:4] - 4'b0001; 
+                    player [9:8] <= 2'b11; //orientation
+                end else if (right && player [7:4] != 1110) begin
+                    player [7:4] <= player [7:4] - 4'b0001;
+                    player [9:0] <= 2'b01; 
                 end else begin
                     next_state = IDLE_STATE;
                 end
             end
 
             ATTACK_STATE: begin   
-                if (A) begin
-                    //logic for the sword to appear in front of the player in the same direction
-                    sword_visible <= 1'b1;
-                    case(player_direction) //display logic to rotate the entity should be used
-                        2'b00: sword_location <= player_location - 8'b0000_1000;
-                        2'b01: sword_location <= player_location + 8'b0000_1000;
-                        2'b01: sword_location <= player_location - 8'b1000_0000;
-                        2'b11: sword_location <= player_location + 8'b1000_0000;
-                        default: sword_location = 8'b0000_0000;
-                    endcase
-                end else
-                    next_state = IDLE_STATE;
+                //logic for the sword to appear in front of the player in the same direction
+                sword_visible <= 1'b1;
+                sword [13:10] <= 4'b0001;
+                if (up && player [3:0] != 0010) begin // ensuring sword doesn't end up outside the border
+                    sword [7:0] <= player [7:0] - 8'b0000_0001;
+                    sword [9:8] <= 2'b00;
+                end else if (down && player [3:0] != 1010) begin
+                    sword [7:0] <= player [7:0] + 8'b0000_0001;
+                    sword [9:8] <= 2'b10;
+                end else if (left && player [7:4] != 0001) begin
+                    sword [7:0] <= player [7:0] - 8'b0001_0000;
+                    sword [9:8] <= 2'b11;
+                end else if (right && player [7:4] != 1110) begin
+                    sword [7:0] <= player [7:0] + 8'b0001_0000;
+                    sword [9:8] <= 2'b01;
+                end
+                next_state = IDLE_STATE;
             end
 
             DEAD: game_over <= 1'b1;
