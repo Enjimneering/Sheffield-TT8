d1348f17d9bad22758f2fbfc880e21ad0ea1b013
Anubhav <anubhav.kumar.avinash@gmail.com>
2024-09-04 12:56:43 +0100

    More fixes

--- src/anu_player.v
+++ src/anu_player.v
@@ -115,14 +115,14 @@ module player (
             MOVE_STATE: begin
                 // Move player based on direction inputs and update orientation
                 if (up && player[3:0] > 4'b0001) begin  // Check boundary for up movement
-                    player[3:0] <= player [7:0] - 8'b0000_0001;  // Move up
+                    player[7:0] <= player [7:0] - 8'b0000_0001;  // Move up
                 end else if (down && player[3:0] < 4'b1011) begin  // Check boundary for down movement
-                    player[3:0] <= player [7:0] - 8'b0000_0001;  // Move down
+                    player[7:0] <= player [7:0] + 8'b0000_0001;  // Move down
                 end else if (left && player[7:4] > 4'b0000) begin  // Check boundary for left movement
-                    player[7:4] <= player [7:0] - 8'b0000_0001;  // Move left
+                    player[7:0] <= player [7:0] - 8'b0000_0001;  // Move left
                     player[9:8] <= 2'b11;  // Update orientation to left
                 end else if (right && player[7:4] < 4'b1111) begin  // Check boundary for right movement
-                    player[7:4] <= player [7:0] - 8'b0000_0001;  // Move right
+                    player[7:0] <= player [7:0] + 8'b0000_0001;  // Move right
                     player[9:8] <= 2'b01;  // Update orientation to right
                 end
                 next_state = IDLE_STATE;  // Return to IDLE after moving
@@ -158,3 +158,4 @@ module player (
         endcase
     end
 endmodule
+    
