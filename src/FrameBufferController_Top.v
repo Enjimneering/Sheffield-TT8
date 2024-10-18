3a23cdcefd98713dc3f34f2979220a571d5aaee4
Kitsudora <bshi12@sheffield.ac.uk>
2024-10-18 14:06:55 +0100

    Update FrameBufferController_Top.v

--- src/FrameBufferController_Top.v
+++ src/FrameBufferController_Top.v
@@ -137,66 +137,66 @@ reg [6:0] set_Pt;
 always@(posedge clk)begin
 
     if (!reset) begin
-    // case (entity_Counter)
-    //     4'd0: begin 
-    //         general_Entity <= {entity_9_Flip,4'b0000};   //position is XXYY!!!!!
-    //         flip_Or_Array_Flag <= 2'b01;
-    //         end
-    //     4'd1:begin
-    //         general_Entity <= {entity_8_Flip,4'b0000}; 
-    //         flip_Or_Array_Flag <= 2'b01;
-    //     end   
-    //     4'd2:begin
-    //         general_Entity <= entity_7_Array;
-    //         flip_Or_Array_Flag <= 2'b10;
-    //     end
-    //     4'd3:begin 
-    //         general_Entity <= {entity_6,4'b0000};
-    //         flip_Or_Array_Flag <= 2'b00;
-    //     end
-    //     4'd4:begin 
-    //         general_Entity <= {entity_5,4'b0000};
-    //         flip_Or_Array_Flag <= 2'b00;
-    //     end
-    //     4'd5:begin 
-    //         general_Entity <= {entity_4,4'b0000};
-    //         flip_Or_Array_Flag <= 2'b00;
-    //     end
-    //     4'd6:begin 
-    //         general_Entity <= {entity_3,4'b0000};
-    //         flip_Or_Array_Flag <= 2'b00;
-    //     end
-    //     4'd7:begin 
-    //         general_Entity <= {entity_2,4'b0000};
-    //         flip_Or_Array_Flag <= 2'b00;
-    //     end
-    //     4'd8: begin
-    //         general_Entity <= {entity_1,4'b0000};
-    //         flip_Or_Array_Flag <= 2'b00;
-    //     end
-        
-    //     default: begin
-    //         general_Entity <= 18'b111111000000000000;
-    //         flip_Or_Array_Flag <= 2'b11;
-    //     end
-    // endcase
-
-        if (set_Pt != 7'b1111111)begin
-            general_Entity[17:4] <= all_Entity[set_Pt+13:set_Pt];
-        end else begin
-            general_Entity <= 18'b111111000000000000;
-        end
-
-        if (set_Pt < 28)begin
+    case (entity_Counter)
+        4'd0: begin 
+            general_Entity <= {entity_9_Flip,4'b0000};   //position is XXYY!!!!!
             flip_Or_Array_Flag <= 2'b01;
-            general_Entity[3:0] <= 4'b0000;
-        end else if(set_Pt == 28) begin
+            end
+        4'd1:begin
+            general_Entity <= {entity_8_Flip,4'b0000}; 
+            flip_Or_Array_Flag <= 2'b01;
+        end   
+        4'd2:begin
+            general_Entity <= entity_7_Array;
             flip_Or_Array_Flag <= 2'b10;
-            general_Entity[3:0] <= entity_7_Array[3:0];
-        end else begin
-            flip_Or_Array_Flag <= 2'b00;
-            general_Entity[3:0] <= 4'b0000;
         end
+        4'd3:begin 
+            general_Entity <= {entity_6,4'b0000};
+            flip_Or_Array_Flag <= 2'b00;
+        end
+        4'd4:begin 
+            general_Entity <= {entity_5,4'b0000};
+            flip_Or_Array_Flag <= 2'b00;
+        end
+        4'd5:begin 
+            general_Entity <= {entity_4,4'b0000};
+            flip_Or_Array_Flag <= 2'b00;
+        end
+        4'd6:begin 
+            general_Entity <= {entity_3,4'b0000};
+            flip_Or_Array_Flag <= 2'b00;
+        end
+        4'd7:begin 
+            general_Entity <= {entity_2,4'b0000};
+            flip_Or_Array_Flag <= 2'b00;
+        end
+        4'd8: begin
+            general_Entity <= {entity_1,4'b0000};
+            flip_Or_Array_Flag <= 2'b00;
+        end
+        
+        default: begin
+            general_Entity <= 18'b111111000000000000;
+            flip_Or_Array_Flag <= 2'b11;
+        end
+    endcase
+
+        // if (set_Pt != 7'b1111111)begin
+        //     general_Entity[17:4] <= all_Entity[set_Pt+13:set_Pt];
+        // end else begin
+        //     general_Entity <= 18'b111111000000000000;
+        // end
+
+        // if (set_Pt < 28)begin
+        //     flip_Or_Array_Flag <= 2'b01;
+        //     general_Entity[3:0] <= 4'b0000;
+        // end else if(set_Pt == 28) begin
+        //     flip_Or_Array_Flag <= 2'b10;
+        //     general_Entity[3:0] <= entity_7_Array[3:0];
+        // end else begin
+        //     flip_Or_Array_Flag <= 2'b00;
+        //     general_Entity[3:0] <= 4'b0000;
+        // end
 
 
         local_Counter_H <= Counter_H_Tile + 1;
