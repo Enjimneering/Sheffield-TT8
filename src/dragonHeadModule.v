261dc6ae5cf8aa7ebb1953cb20e196dccaff82c3
Kitsudora <bshi12@sheffield.ac.uk>
2024-10-25 14:24:07 +0100

    Syntax Fix and decoupling

--- src/dragonHeadModule.v
+++ src/dragonHeadModule.v
@@ -15,90 +15,85 @@
 module DragonHead ( 
     input clk,
     input reset,
-    input player_pos,
-    input dragon_pos,
+    input [7:0] player_pos,
+  
+    input vsync,
+
+    output reg [1:0] dragon_direction,
+    output reg [7:0] dragon_pos,
+    output reg [5:0] movement_counter// Counter for delaying dragon's movement otherwise sticks to player
 );
 
    
+reg [3:0] dragon_x;
+reg [3:0] dragon_y;
 
 
-reg [1:0] dragon_direction;
-reg [3:0] dragon_sprite;
-reg [3:0] player_y;
-reg [3:0] player_x;
-wire [3:0] dragon_x = ;
-wire [3:0] dragon_y;
-
-
-reg [3:0] next_x;
-reg [3:0] next_y;
-reg [7:0] target_location;
 reg [3:0] dx; //difference
 reg [3:0] dy;
 reg [3:0] sx; //figuring out direction in axis
 reg [3:0] sy;
-reg [5:0] movement_counter = 0;  // Counter for delaying dragon's movement otherwise sticks to player
-reg [1:0] NextDirection;
+// reg [5:0] movement_counter = 0;  // Counter for delaying dragon's movement otherwise sticks to player
+
 
 
 // Movement logic , uses bresenhams line algorithm
 always @(posedge vsync) begin
+  if (~reset)begin
     if (movement_counter < 6'd10) begin
         movement_counter <= movement_counter + 1;
     end else begin
         movement_counter <= 0;
 
         // Store the current position before updating , used later
-        dragon_pos <= target_location;
+        dragon_x <= dragon_pos[7:4];
+        dragon_y <= dragon_pos[3:0];
 
-        // Follow the player
-        /*
-        player_y <= player_pos[3:0];
-        player_x <= player_pos[7:4];
-    */
         // Calculate the differences between dragon and player
-        dx <= player_pos[7:4] - dragon_x ;
+        dx <= player_pos[7:4] - dragon_x;
         dy <= player_pos[3:0] - dragon_y ;
-        sx <= (dragon_x< player_pos[7:4]) ? 1 : -1; // Direction in axis
-        sy <= (dragon_y< player_pos[3:0]) ? 1 : -1; 
+        sx <= (dragon_x < player_pos[7:4]) ? 1 : -1; // Direction in axis
+        sy <= (dragon_y < player_pos[3:0]) ? 1 : -1; 
 
         // Move the dragon towards the player if it's not adjacent
         if (dx >= 1 || dy >= 1) begin
+        // Update dragon position only if it actually moves , keeps flickering
             if (dx >= dy) begin //prioritize movement
-                
                 dragon_x <= dragon_x + sx;
                 dragon_y <= dragon_y;
             end else begin
-                
                 dragon_x <= dragon_x;
                 dragon_y <= dragon_y + sy;
             end
 
-            // Update dragon position only if it actually moves , keeps flickering
-
-              // dragon_x = next_x;
-              // dragon_y = next_y;
             if (dragon_x > dragon_pos[7:4])
-              NextDirection = 2'b01;   // Move right
+              dragon_direction <= 2'b01;   // Move right
             else if (dragon_x < dragon_pos[7:4])
-              NextDirection = 2'b11;   // Move left
+              dragon_direction <= 2'b11;   // Move left
             else if (dragon_y > dragon_pos[3:0])
-              NextDirection = 2'b10;   // Move down
+              dragon_direction <= 2'b10;   // Move down
             else if (dragon_y < dragon_pos[3:0])
-              NextDirection = 2'b00;   // Move up
+              dragon_direction <= 2'b00;   // Move up
 
-              // dragon_direction <= NextDirection(dragon_pos, {dragon_x, dragon_y});
-              dragon_direction <= NextDirection;
-
-              // Update the next location
-              target_location <= {dragon_x, dragon_y};
+            // Update the next location
+            dragon_pos <= {dragon_x, dragon_y};
 
         end else begin
             // stop moving when the dragon is adjacent to the player 
             dragon_x <= dragon_x; 
             dragon_y <= dragon_y; 
         end
-end
+    end
+  end else begin
+    dragon_x <= 0;
+    dragon_y <= 0;
+    movement_counter <= 0;
+    dragon_pos <= 0;
+    dx <= 0; //difference
+    dy <= 0;
+    sx <= 0; //figuring out direction in axis
+    sy <= 0;
+  end
 end
 
 
